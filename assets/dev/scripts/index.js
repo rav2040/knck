063f0526d6edbb99cb6e9dff91cf18236ab21f1d
rav2040 <rav2040@gmail.com>
2020-08-23 16:12:55 +1200

    Split assets dir into 'dev' and 'prod'

---
+++ assets/dev/scripts/index.js
@@ -0,0 +1,26 @@
+const BASE_URI = 'http://localhost:3000';
+
+document
+  .getElementById('form')
+  .addEventListener('submit', async event => {
+    event.preventDefault();
+    const { value } = document.getElementById('url');
+    const { shortUrl } = await postData(value);
+    const resultLink = document.getElementById('result-link');
+    resultLink.setAttribute('href', shortUrl);
+    resultLink.innerHTML = shortUrl.replace(/^https?:\/\//, '');
+    const resultContainer = document.getElementById('result-container');
+    resultContainer.classList.remove('invisible');
+  });
+
+async function postData(url) {
+  const response = await fetch(`${BASE_URI}/new?url=${url}`, {
+    method: 'POST',
+    mode: 'same-origin',
+    cache: 'no-cache',
+    redirect: 'error',
+    referrerPolicy: 'no-referrer',
+  });
+
+  return await response.json();
+}
