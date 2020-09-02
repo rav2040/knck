c1574c445337cc2b56d9a8c47fa9ae46778ba429
rav2040 <rav2040@gmail.com>
2020-09-03 10:13:06 +1200

    Add default AWS region to constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
-export const DEFAULT_DB_ENDPOINT = 'http://localhost:8000';
 export const DB_TABLE_NAME = 'KnckUrls';
+export const DEFAULT_DB_ENDPOINT = 'http://localhost:8000';
+export const DEFAULT_AWS_REGION = 'us-west-1';
 export const DEFAULT_DB_TTL = 2_592_000;
