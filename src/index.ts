31ea41218472f29b2cafa2a74813b14d966e2d3d
rav2040 <rav2040@gmail.com>
2020-09-02 16:14:05 +1200

    Refactor database initialization

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,13 @@ import { tuft, createBodyParser } from 'tuft';
 import { createHelmetPrehandler } from '@tuft/helmet-prehandler';
 import { createViewResponder } from '@tuft/view-responder';
 import { join } from 'path';
-import { createDbInstance } from './db';
+import { createDbClient } from './db';
 import { create } from './controllers/create';
 import { redirect } from './controllers/redirect';
 import { notFoundResponse } from './error-responses';
 
-const MAX_BODY_SIZE = 2048; // bytes
-
 if (process.env.NODE_ENV !== 'development' && process.env.NODE_ENV !== 'production') {
-  const err = new Error('\'PORT\' environment variable must be set to \'development\' or \'production\'.');
+  const err = new Error('\'NODE_ENV \' environment variable must be set to \'development\' or \'production\'.');
   console.error(err);
   process.exit(1);
 }
@@ -27,50 +25,53 @@ if (process.env.PORT === undefined) {
   process.exit(1);
 }
 
+const MAX_BODY_SIZE = 2048; // bytes
+
 const host = process.env.HOST;
 const port = process.env.PORT;
 const assetsDir = join(__dirname, '../public');
 
 async function init() {
-  const db = await createDbInstance();
+  try {
+    const dbClient = await createDbClient();
 
-  if (!db) {
-    const err = Error('Unable to create database instance.');
+    const app = tuft({
+      preHandlers: [
+        createBodyParser('urlEncoded', MAX_BODY_SIZE),
+        createHelmetPrehandler({
+          hidePoweredBy: false,
+          ieNoOpen: false,
+          frameguard: false,
+        }),
+      ],
+      responders: [
+        createViewResponder('ejs', 'views'),
+      ],
+    });
+
+    // Serve static web assets from the root path.
+    await app.static('/', assetsDir);
+
+    app
+      .set('GET /', {
+        render: 'index',
+        data: { shortUrl: null },
+      })
+      .set('POST /', create.bind(null, dbClient))
+      .set('GET /{hash}', redirect.bind(null, dbClient))
+      .set('/{**}', notFoundResponse)
+      .onError(console.error);
+
+    const server = app.createServer({ host, port });
+    const address = await server.start();
+
+    console.log(`Server is listening at http://${address.host}:${address.port}`);
+  }
+
+  catch (err) {
     console.error(err);
     process.exit(1);
   }
-
-  const app = tuft({
-    preHandlers: [
-      createBodyParser('urlEncoded', MAX_BODY_SIZE),
-      createHelmetPrehandler({
-        hidePoweredBy: false,
-        ieNoOpen: false,
-        frameguard: false,
-      }),
-    ],
-    responders: [
-      createViewResponder('ejs', 'views'),
-    ],
-  });
-
-  // Serve static web assets from the root path.
-  await app.static('/', assetsDir);
-
-  app
-    .set('GET /', {
-      render: 'index',
-      data: { shortUrl: null },
-    })
-    .set('POST /', create.bind(null, db))
-    .set('GET /{hash}', redirect.bind(null, db))
-    .set('/{**}', notFoundResponse)
-    .onError(console.error);
-
-  const server = app.createServer({ host, port });
-  const address = await server.start();
-
-  console.log(`Server is listening at http://${address.host}:${address.port}`);
 }
 
 init();
