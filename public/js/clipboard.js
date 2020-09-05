b219a376f57ca331d1eb4dd825d821874440be3e
rav2040 <rav2040@gmail.com>
2020-09-06 11:37:19 +1200

    Remove focus from selected copy button

--- public/js/clipboard.js
+++ public/js/clipboard.js
@@ -1,13 +1,16 @@
-const shortUrl = document.getElementById('hidden-url-container')
-  .innerText
-  .trim();
 const copyButton = document.getElementById('copy-btn');
 copyButton.onclick = () => {
-  copyButton.removeAttribute('id');
-  copyButton.classList.remove('btn-primary');
-  copyButton.classList.add('btn-success');
-  copyButton.innerHTML = 'Copied';
+  const shortUrl = document.getElementById('hidden-url-container')
+    .innerText
+    .trim();
   navigator.clipboard.writeText(shortUrl)
-    .then()
+    .then(() => {
+      copyButton.removeAttribute('id');
+      copyButton.setAttribute('id', 'copy-btn');
+      copyButton.classList.remove('btn-primary');
+      copyButton.classList.add('btn-success');
+      copyButton.innerHTML = 'Copied';
+      copyButton.blur();
+    })
     .catch(console.error);
 };
