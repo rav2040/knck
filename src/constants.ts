f8fa9108d563ba4914388bbec761aff17c8f1e58
rav2040 <rav2040@gmail.com>
2020-09-07 15:49:23 +1200

    Remove default AWS region and endpoint values

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,2 @@
 export const DB_TABLE_NAME = 'KnckUrls';
-export const DEFAULT_DB_ENDPOINT = 'http://localhost:8000';
-export const DEFAULT_AWS_REGION = 'us-west-1';
-export const DEFAULT_DB_TTL = 2_592_000;
+export const DB_ITEM_TTL = 2_592_000; // in seconds
